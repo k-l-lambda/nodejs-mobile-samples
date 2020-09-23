86c71fd73d0a91fd9c0c1e3aafbdef9a28555697
k.l.lambda <k.l.lambda@gmail.com>
2020-09-23 17:12:31 +0800

    native node module added.

--- android/native-gradle-node-folder/app/src/main/assets/nodejs-project/main.js
+++ android/native-gradle-node-folder/app/src/main/assets/nodejs-project/main.js
@@ -1,7 +1,9 @@
 var http = require('http');
 var leftPad = require('left-pad');
+var fc = require('./function-call');
 var versions_server = http.createServer( (request, response) => {
-  response.end('Versions: ' + JSON.stringify(process.versions) + ' left-pad: ' + leftPad(42, 5, '0'));
+	//response.end('Versions: ' + JSON.stringify(process.versions) + ' left-pad: ' + leftPad(42, 5, '0'));
+	response.end('FC: ' + fc.hello().toString());
 });
 versions_server.listen(3000);
 console.log('The node project has started.');
